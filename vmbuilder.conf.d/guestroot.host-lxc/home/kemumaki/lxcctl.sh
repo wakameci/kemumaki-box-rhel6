971a23c2de9835f675bbddbb5764a297c5fd4324
Masahito Yoshida <masahito@axsh.net>
2014-12-13 02:49:42 +0900

    run mknod in container.

--- vmbuilder.conf.d/guestroot.host-lxc/home/kemumaki/lxcctl.sh
+++ vmbuilder.conf.d/guestroot.host-lxc/home/kemumaki/lxcctl.sh
@@ -136,3 +136,19 @@ install_lxc_udev ${ctid}
 
 lxc-create -f /etc/lxc/${ctid}.conf -n ${ctid}
 lxc-start -n ${ctid} -d -l DEBUG -o /var/log/lxc/${ctid}.log
+
+### add device
+
+#lxc-device -n ${ctid} add /dev/kvm
+#lxc-device -n ${ctid} add /dev/net/tun
+
+# > PTY allocation request failed on channel 0
+#lxc-device -n ${ctid} add /dev/ptmx
+
+# /dev/loopX and /dev/dm-X
+for i in {0..127}; do
+lxc-attach -n ${ctid} -- bash -ex <<-EOS
+  [[ -b /dev/loop${i} ]] || mknod /dev/loop${i} -m 660 b   7 ${i}
+  [[ -b /dev/dm-${i}  ]] || mknod /dev/dm-${i}  -m 660 b 253 ${i}
+EOS
+done
