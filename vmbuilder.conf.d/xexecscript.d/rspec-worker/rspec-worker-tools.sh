3d3f6b3b655c5b4c218d7a62cdf0b75a4460494b
Masahito Yoshida <masahito@axsh.net>
2015-03-29 18:27:40 +0900

    remove duplicated epel installation.

--- vmbuilder.conf.d/xexecscript.d/rspec-worker/rspec-worker-tools.sh
+++ vmbuilder.conf.d/xexecscript.d/rspec-worker/rspec-worker-tools.sh
@@ -25,12 +25,4 @@ chroot $1 $SHELL -ex <<'EOS'
     rpmdevtools
   "
   yum install -y --disablerepo=updates ${addpkgs}
-
-  rpm -qa epel-release* | egrep -q epel-release || {
-    rpm -Uvh http://dlc.wakame.axsh.jp.s3-website-us-east-1.amazonaws.com/epel-release
-
-    # in order escape below error
-    # > Error: Cannot retrieve metalink for repository: epel. Please verify its path and try again
-    yum install -y ca-certificates
-  }
 EOS
