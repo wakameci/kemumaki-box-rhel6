4f7cff54c4ef3faf138eb91554f28adccac8538c
Masahito Yoshida <masahito@axsh.net>
2015-08-14 01:15:56 +0900

    dlc.wakame.axsh.jp -> dlc2.wakame.axsh.jp

--- vmbuilder.conf.d/xexecscript.d/host-lxc/zz_kemukins-template.sh
+++ vmbuilder.conf.d/xexecscript.d/host-lxc/zz_kemukins-template.sh
@@ -10,7 +10,7 @@ declare chroot_dir=$1
 chroot $1 $SHELL -ex <<'EOS'
   mkdir -p /lxc/template/cache
 
-  until curl -fSkL -o /lxc/template/cache/vz.kemumaki.x86_64.tar.gz http://dlc.wakame.axsh.jp/wakameci/kemumaki-box-rhel6/current/vz.kemumaki.x86_64.tar.gz; do
+  until curl -fSkL -o /lxc/template/cache/vz.kemumaki.x86_64.tar.gz http://dlc2.wakame.axsh.jp/wakameci/kemumaki-box-rhel6/current/vz.kemumaki.x86_64.tar.gz; do
     sleep 1
   done
 EOS
--- vmbuilder.conf.d/xexecscript.d/host-vz/zz_kemukins-template.sh
+++ vmbuilder.conf.d/xexecscript.d/host-vz/zz_kemukins-template.sh
@@ -8,7 +8,7 @@ set -e
 declare chroot_dir=$1
 
 chroot $1 $SHELL -ex <<'EOS'
-  until curl -fSkL -o /vz/template/cache/vz.kemumaki.x86_64.tar.gz http://dlc.wakame.axsh.jp/wakameci/kemumaki-box-rhel6/current/vz.kemumaki.x86_64.tar.gz; do
+  until curl -fSkL -o /vz/template/cache/vz.kemumaki.x86_64.tar.gz http://dlc2.wakame.axsh.jp/wakameci/kemumaki-box-rhel6/current/vz.kemumaki.x86_64.tar.gz; do
     sleep 1
   done
 EOS
